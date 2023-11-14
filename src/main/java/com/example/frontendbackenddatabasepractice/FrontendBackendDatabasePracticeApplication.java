bfedc28372f6d870cad5ef9fb96555e2aea94370
minseok <alstjr706@naver.com>
2023-11-14 16:25:58 +0900

    chore : (연동 연습) mybatis 연결

--- src/main/java/com/example/frontendbackenddatabasepractice/FrontendBackendDatabasePracticeApplication.java
+++ src/main/java/com/example/frontendbackenddatabasepractice/FrontendBackendDatabasePracticeApplication.java
@@ -1,13 +1,15 @@
 package com.example.frontendbackenddatabasepractice;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.frontendbackenddatabasepractice")
 public class FrontendBackendDatabasePracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FrontendBackendDatabasePracticeApplication.class, args);
     }
-
 }
